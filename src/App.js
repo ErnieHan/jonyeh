7e4a035a29dc4b79c8a72d25065a36d3f41a435d
葉宗翰 <02090560@me.mcu.edu.tw>
2019-03-10 19:37:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import "./App.scss";
 import Header from "./Component/Header";
 import Home from "./Pages/Home";
 import Footer from "./Component/Footer";
+import Loading from "./Component/Loading";
 
 const Page404 = () => {
   return <div>Page404</div>;
 };
 
 class App extends Component {
+  state = {
+    isLoading: true
+  };
+  componentDidMount = () => {
+    window.addEventListener("load", () =>
+      this.setState({
+        isLoading: false
+      })
+    );
+  };
   render() {
     return (
       <HashRouter>
         <Layout>
           <Header />
+          {this.state.isLoading && <Loading />}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route component={Page404} />
